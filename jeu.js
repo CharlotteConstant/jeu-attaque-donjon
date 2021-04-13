f73f8575ae42c5953351559459bfc54bf342685c
CharlotteConstant <constantcharlotte8@orange.fr>
2021-04-13 14:27:43 +0200

    modif

--- jeu.js
+++ jeu.js
@@ -1,9 +1,5 @@
-console.log(
-  `Bienvenue dans le donjon ou vous allez affronter le maitre des lieux`
-);
-
 let arme = {
-  bois: 3,
+  bois: 2,
   fer: 5,
   magique: 10,
 };
@@ -15,74 +11,80 @@ let armure = {
 };
 
 let player = {
-  pointVie: 40,
+  pointVie: 20,
   dommages: "",
   armure: "",
 };
 
 let maitreDonjon = {
-  pointVie: 50,
+  pointVie: 30,
   dommages: 6,
   armure: 4,
 };
 
 let armeUser;
 
-while (armeUser != `bois` && armeUser != `fer` && armeUser != `magique`) {
-  armeUser = prompt(`Veuillez choisir votre arme: bois, fer ou magique`);
+let form = document.querySelector("form");
+let log = document.querySelector("#log");
+form.addEventListener("submit", logSubmit);
+
+function logSubmit(event) {
+  let checked1 = document.querySelector('input[name="choixArme"]:checked');
+  let checked2 = document.querySelector('input[name="choixArmure"]:checked');
+  let armeP = checked1.dataset.title;
+  let armureP = checked2.dataset.title;
+  let value1 = checked1.value;
+  let value2 = checked2.value;
+  event.preventDefault();
+  log.textContent = `Vous avez choisi une arme ${armeP} qui infligera ${value1} de dégâts et une armure ${armureP} qui vous protegera de ${value2} points`;
+
+  let tour = document.querySelector("#tour ul");
+  combat(maitreDonjon, player);
+  resultat();
 }
 
-console.log(`Vous avez choisi arme ${armeUser}`);
+//combat
 
-let armureUser;
+function combat(maitreDonjon, player) {
+  let play = 1;
+  let ul = document.querySelector(".list");
+  let li = document.createElement("li");
+  ul.appendChild(li);
+  let checked1 = document.querySelector('input[name="choixArme"]:checked');
+  let checked2 = document.querySelector('input[name="choixArmure"]:checked');
+  let value1 = checked1.value;
+  let value2 = checked2.value;
+  player.dommages = value1;
+  player.armure = value2;
+  let degatsMaitreDonjon = player.dommages - maitreDonjon.armure;
+  let degatsPlayer = maitreDonjon.dommages - player.armure;
 
-while (armureUser != `bois` && armureUser != `fer` && armureUser != `magique`) {
-  armureUser = prompt("Veuillez choisir votre armure: bois, fer ou magique");
-}
-
-console.log(`Vous avez choisi armure ${armureUser}`);
-
-console.log("le combat va commencer");
-
-// combat
-let play = 1;
-
-player.dommages = arme[armeUser];
-player.armure = armure[armureUser];
-
-let degatsMaitreDonjon = player.dommages - maitreDonjon.armure;
-let degatsPlayer = maitreDonjon.dommages - player.armure;
-
-while (maitreDonjon.pointVie > 0 && player.pointVie > 0) {
-  if (play % 2 == 0) {
-    maitreDonjon.pointVie = maitreDonjon.pointVie - degatsMaitreDonjon;
-    if (degatsMaitreDonjon > 0) {
-      console.log(
-        `Vous attaquez et infligez ${degatsMaitreDonjon} de dégâts, il reste ${maitreDonjon.pointVie} de points de vie au maitre du donjon`
-      );
+  while (maitreDonjon.pointVie > 0 && player.pointVie > 0) {
+    if (play % 2 == 0) {
+      maitreDonjon.pointVie = maitreDonjon.pointVie - degatsMaitreDonjon;
+      if (degatsMaitreDonjon > 0) {
+        li.innerHTML += `Vous attaquez et infligez ${degatsMaitreDonjon} de dégâts, il reste ${maitreDonjon.pointVie} de points de vie au maitre du donjon <br>`;
+      } else {
+        li.innerHTML += `Vous n'infligez pas de dégâts au maitre du donjon <br>`;
+      }
     } else {
-      console.log(`Vous n'infligez pas de dégâts au maitre du donjon`);
+      player.pointVie = player.pointVie - degatsPlayer;
+      if (degatsPlayer > 0) {
+        li.innerHTML += `le maitre du donjon vous attaque et vous inflige ${degatsPlayer} de dégâts, il vous reste ${player.pointVie} point de vie<br>`;
+      } else {
+        li.innerHTML += `le maitre du donjon ne vous inflige pas de dégâts<br>`;
+      }
     }
-  } else {
-    player.pointVie = player.pointVie - degatsPlayer;
-    if (degatsPlayer > 0) {
-      console.log(
-        `le maitre du donjon vous attaque et vous inflige ${degatsPlayer} de dégâts, il vous reste ${player.pointVie} point de vie`
-      );
-    } else {
-      console.log(`le maitre du donjon ne vous inflige pas de dégâts`);
-    }
+    play++;
   }
-  play++;
 }
 
 //fin du jeu
-if (maitreDonjon.pointVie > 0) {
-  console.log(
-    `Vous avez perdu contre le maitre du Donjon qui est bien trop fort pour vous!`
-  );
-} else if (player.pointVie > 0) {
-  console.log(
-    `Bravo, vous avez gagné contre le maitre du Donjon! Recommencez pour être sur que ce n'est pas la chance du débutant`
-  );
+function resultat() {
+  let resultat = document.querySelector("#resultat h2");
+  if (maitreDonjon.pointVie > 0) {
+    resultat.innerHTML = `Vous avez perdu contre le maitre du Donjon qui est bien trop fort pour vous!`;
+  } else if (player.pointVie > 0) {
+    resultat.innerHTML = `Bravo, vous avez gagné contre le maitre du Donjon! Recommencez pour être sur que ce n'est pas la chance du débutant!`;
+  }
 }
